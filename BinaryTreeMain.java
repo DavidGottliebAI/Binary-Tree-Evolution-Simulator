ed5925baba9969e42a64887777cff1deee20dede
David Gottlieb <42564419+DavidGottliebAI@users.noreply.github.com>
2021-03-31 00:13:48 -0400

    Delete BinaryTreeMain.java

--- BinaryTreeMain.java
+++
@@ -1,9 +0,0 @@
-
-public class BinaryTreeMain {
-	
-	public static void main(String[] args) throws Exception {    // construct the binary tree given in question    
-		new BinaryTreeVisualizer();
-	}
-}
-	
-
